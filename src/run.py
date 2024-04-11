37c14f22997fc293f9a2ff786db0fb79adf8e2d7
Kirill Zakharov <KYZakharov@stud.kpfu.ru>
2024-04-12 00:39:10 +0300

    readme

---
+++ src/run.py
@@ -0,0 +1,11 @@
+from simplex_method import *
+
+try:
+    prices, norms, limit = read_function('../data/data.txt')
+    test_simplex = SimplexMethod(norms, limit, prices)
+    test_simplex.solve()
+
+except ValueError as e:
+    print("Произошла ошибка:", e)
+
+
