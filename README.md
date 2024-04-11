597cd272031c8252b59c3e6827105c66dfccae28
Kirill Zakharov <KYZakharov@stud.kpfu.ru>
2024-04-12 00:09:04 +0300

    порядок по папкам

---
+++ README.md
@@ -0,0 +1,5 @@
+# Simplex method
+
+**Данный алгоритм решает задачи вида:**
+
+![img.png](images%2Fimg.png)
--- data+run.py
+++
@@ -1,9 +0,0 @@
-from simplex_method import *
-
-try:
-    prices, norms, limit = read_function('data.txt')
-    test_simplex = SimplexMethod(norms, limit, prices)
-    test_simplex.solve()
-
-except ValueError as e:
-    print("Произошла ошибка:", e)
